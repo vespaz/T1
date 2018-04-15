40dc8276e743150acea377a538a8d436b642f6ee
vespaz <vespazum@gmail.com>
2018-04-15 13:58:42 -0300

    Finalizado!

--- t1.php
+++ t1.php
@@ -2,15 +2,15 @@
 
 $conn = mysqli_connect("localhost", "root", "usbw", "loja");
 
-$select = "SELECT * FROM loja_cadastro JOIN loja_cidade ON loja_cadastro.cod_cidade=loja_cidade.id_cidade JOIN loja_estado ON loja_cidade.cod_estado=loja_estado.id_estado";
+$select = "SELECT * FROM cadastro JOIN cidade ON cadastro.cod_cidade=cidade.id_cidade JOIN estado ON cidade.cod_estado=estado.id_estado";
 
 $query = mysqli_query($conn,$select);
 
 
 while($linha=mysqli_fetch_array($query)){
-	echo "<pre>";
-		print_r($linha);
-	echo "</pre>";
+
+	print_r($linha);
+	
 	
 	echo "<br /><br />";
 
